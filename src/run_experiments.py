5754290d2c5c4cff6419d0cb4a8933a11c9b24b1
Herbje <59506700+Herbje@users.noreply.github.com>
2025-03-30 11:32:34 +0200

    Update run_experiments.py

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -1,10 +1,13 @@
+from logging import warning
 import os.path
 from pathlib import Path
+import torch
 
 import pandas as pd
 import pyterrier as pt
 
-pt.init()
+pt.java.init()
+pt.java.set_log_level('DEBUG')
 
 from pyterrier.terrier import Retriever
 from pyterrier_colbert.indexing import ColBERTIndexer
@@ -30,6 +33,15 @@ def bm25_fair() -> Retriever:
 
     return pt.terrier.Retriever(index, wmodel="BM25")
 
+index = 0
+def wrapper(iterator):
+    global index
+    for i in iterator:
+        index += 1
+        if len(i['text']) > 0 and not i['text'].isspace():
+            yield i
+        else:
+            warning(f"Empty text at index {index - 1} in corpus")
 
 def colbert_fair():
     dataset = pt.get_dataset(f"irds:{FAIR_DATASET_NAME}")
@@ -39,23 +51,21 @@ def colbert_fair():
     index_path = Path.cwd() / ".." / "data" / index_name
     index_properties = index_path / "data.properties"
     # Checkpoint is not trained for trec dataset specifically, may need to try and train it later
-    checkpoint = "http://www.dcs.gla.ac.uk/~craigm/ecir2021-tutorial/colbert_model_checkpoint.zip"
+    checkpoint = "http://www.dcs.gla.ac.uk/~craigm/colbert.dnn.zip"
 
     if not os.path.exists(index_properties):
         print("Creating COLBERT index")
-        indexer = ColBERTIndexer(checkpoint, str(index_path), index_name, chunksize=3)
-        indexer.index(dataset.get_corpus_iter())
+        indexer = ColBERTIndexer(checkpoint, str(index_path), index_name, chunksize=1, gpu=torch.cuda.is_available())
+        indexer.index(wrapper(dataset.get_corpus_iter()))
 
-    index = (str(index_path), index_name)
-
-    return ColBERTFactory(checkpoint, *index)
+    return indexer.ranking_factory()
 
 
 if __name__ == '__main__':
     pd.set_option('display.width', 200)
     pd.set_option('display.max_columns', 6)
     bm25 = bm25_fair()
-    print(bm25.search("test"))
-    # colbert = colbert_fair()
-    # dense_e2e = colbert.end_to_end()
-    # dense_e2e.search("chemical reactions").head(5)
+    print(bm25.search("test"), '\n\n\n\n')
+    colbert = colbert_fair()
+    dense_e2e = colbert.end_to_end()
+    dense_e2e.search("chemical reactions").head(5)
