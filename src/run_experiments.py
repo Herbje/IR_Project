dbd3bbd5fec5992a99ec6bff90c611c27d7061d4
07Liyang <152295290+07Liyang@users.noreply.github.com>
2025-03-31 13:42:54 +0200

    Use run_experiments.py from lirme branch (overwrite)

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -1,62 +1,70 @@
+from logging import warning
 import os.path
 from pathlib import Path
+import torch
 
 import pandas as pd
 import pyterrier as pt
 
-pt.init()
+pt.java.init()
+# pt.java.set_log_level('DEBUG')
 
 from pyterrier.terrier import Retriever
 from pyterrier_colbert.indexing import ColBERTIndexer
 from pyterrier_colbert.ranking import ColBERTFactory
 
-FAIR_DATASET_NAME = "trec-fair/2021"
-# FAIR_TRAIN_DATASET_NAME = "trec-fair/2021/train"
-# FAIR_EVAL_DATASET_NAME = "trec-fair/2021/eval"
+VASWANI_DATASET_NAME = "vaswani"
 
-def bm25_fair() -> Retriever:
-    dataset = pt.get_dataset(f"irds:{FAIR_DATASET_NAME}")
+def bm25_vaswani() -> Retriever:
+    dataset = pt.get_dataset(f"irds:{VASWANI_DATASET_NAME}")
 
-    index_path = Path.cwd() / ".." / "data" / "trec-fair-bm25"
+    index_path = Path(__file__).parent / ".." / "data" / "vaswani-bm25"
     index_properties = index_path / "data.properties"
 
     if os.path.exists(index_properties):
         print(f"Using existing index {index_path.absolute()}")
-        # index = pt.IndexFactory.of(index_path)
-        index = pt.IndexFactory.of(str(index_path)) 
+        index = pt.IndexFactory.of(str(index_path))
     else:
         indexer = pt.IterDictIndexer(str(index_path))
         indexref = indexer.index(dataset.get_corpus_iter())
         index = pt.IndexFactory.of(indexref)
+    return index
 
-    return pt.terrier.Retriever(index, wmodel="BM25")
+ind = 0
+def wrapper(iterator):
+    global ind
+    for i in iterator:
+        ind += 1
+        if len(i['text']) > 0 and not i['text'].isspace():
+            yield i
+        else:
+            warning(f"Empty text at index {ind - 1} in corpus")
 
+def colbert_vaswani():
+    dataset = pt.get_dataset(f"irds:{VASWANI_DATASET_NAME}")
 
-def colbert_fair():
-    dataset = pt.get_dataset(f"irds:{FAIR_DATASET_NAME}")
-
-    index_name = "trec-fair-colbert"
-
-    index_path = Path.cwd() / ".." / "data" / index_name
-    index_properties = index_path / "data.properties"
+    index_name = "vaswani-colbert"
+    index_path = Path(__file__).parent / ".." / "data" / index_name
+    index_name_check = index_path / index_name / 'ivfpq.faiss'
     # Checkpoint is not trained for trec dataset specifically, may need to try and train it later
-    checkpoint = "http://www.dcs.gla.ac.uk/~craigm/ecir2021-tutorial/colbert_model_checkpoint.zip"
+    checkpoint = "http://www.dcs.gla.ac.uk/~craigm/colbert.dnn.zip"
 
-    if not os.path.exists(index_properties):
+    if not os.path.exists(index_name_check):
         print("Creating COLBERT index")
-        indexer = ColBERTIndexer(checkpoint, str(index_path), index_name, chunksize=3)
-        indexer.index(dataset.get_corpus_iter())
+        indexer = ColBERTIndexer(checkpoint, str(index_path), index_name, chunksize=3, gpu=torch.cuda.is_available())
+        indexer.index(wrapper(dataset.get_corpus_iter()))
+        if os.path.exists(Path(index_path / index_name / 'ivfpq.100.faiss')):
+            Path(index_path / index_name / 'ivfpq.100.faiss').rename(index_name_check)
+    index = ColBERTFactory(checkpoint, str(index_path), index_name, gpu=torch.cuda.is_available())
 
-    index = (str(index_path), index_name)
-
-    return ColBERTFactory(checkpoint, *index)
+    return index
 
 
 if __name__ == '__main__':
     pd.set_option('display.width', 200)
     pd.set_option('display.max_columns', 6)
-    bm25 = bm25_fair()
-    bm25.search("test")
-    # colbert = colbert_fair()
-    # dense_e2e = colbert.end_to_end()
-    # dense_e2e.search("chemical reactions").head(5)
+    bm25 = pt.terrier.Retriever(bm25_vaswani(), wmodel="BM25")
+    print(bm25.search("test"), '\n\n\n\n')
+    colbert = colbert_vaswani()
+    colbert_e2e = colbert.end_to_end()
+    print((colbert_e2e % 5).search("chemical reactions"))
