10b629c999d6a5da40f3c693f0ac698a062bda7a
Jim van Vliet <j.i.m.vanvliet@student.tudelft.nl>
2025-04-03 12:58:36 +0200

    Add monot5 ranker and vaswani dataset

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -5,6 +5,7 @@ import torch
 
 import pandas as pd
 import pyterrier as pt
+from pyterrier_t5 import MonoT5ReRanker
 
 pt.java.init()
 # pt.java.set_log_level('DEBUG')
@@ -14,6 +15,8 @@ from pyterrier_colbert.indexing import ColBERTIndexer
 from pyterrier_colbert.ranking import ColBERTFactory
 
 FAIR_DATASET_NAME = "trec-fair/2021"
+VASWANI_DATASET = "vaswani"
+
 
 def index_fair() -> Retriever:
     dataset = pt.get_dataset(f"irds:{FAIR_DATASET_NAME}")
@@ -31,7 +34,27 @@ def index_fair() -> Retriever:
 
     return index
 
+
+def index_vaswani() -> Retriever:
+    dataset = pt.get_dataset(f"irds:{VASWANI_DATASET}")
+
+    index_path = Path(__file__).parent / ".." / "data" / f"{VASWANI_DATASET}-index"
+    index_properties = index_path / "data.properties"
+
+    if os.path.exists(index_properties):
+        print(f"Using existing index {index_path.absolute()}")
+        index = pt.IndexFactory.of(str(index_path))
+    else:
+        indexer = pt.IterDictIndexer(str(index_path))
+        indexref = indexer.index(dataset.get_corpus_iter())
+        index = pt.IndexFactory.of(indexref)
+
+    return index
+
+
 ind = 0
+
+
 def wrapper(iterator):
     global ind
     for i in iterator:
@@ -40,6 +63,7 @@ def wrapper(iterator):
             yield i
         else:
             warning(f"Empty text at index {ind - 1} in corpus")
+
 
 def colbert_fair():
     dataset = pt.get_dataset(f"irds:{FAIR_DATASET_NAME}")
@@ -62,12 +86,23 @@ def colbert_fair():
     return index
 
 
+def monot5():
+    mono = MonoT5ReRanker()
+
+    dataset = pt.get_dataset(f"irds:{VASWANI_DATASET}")
+    index = index_vaswani()
+    bm25 = pt.BatchRetrieve(index, wmodel="BM25")
+    return bm25 >> pt.text.get_text(dataset, "text") >> mono
+
+
 if __name__ == '__main__':
     pd.set_option('display.width', 200)
     pd.set_option('display.max_columns', 6)
 
-    bm25 = pt.terrier.Retriever(index_fair(), wmodel="BM25")
-    print(bm25.search("test"), '\n\n\n\n')
+    bm25 = pt.terrier.Retriever(index_vaswani(), wmodel="BM25")
+    print(bm25.search("relativity"), '\n\n\n\n')
+    #
+    # tf_idf = pt.terrier.Retriever(index_fair(), wmodel="TF_IDF")
+    # print(tf_idf.search("test"))
 
-    tf_idf = pt.terrier.Retriever(index_fair(), wmodel="TF_IDF")
-    print(tf_idf.search("test"))
+    print(monot5().search("relativity"))
